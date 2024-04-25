c2fc0d362eb09d4eaae28665f4b8d681ee084bfb
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-04-25 15:00:55 +0300

    add content in home page and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-
+import { NavLink } from 'react-router-dom';
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <main className={styles.wrap_hero}>
+      <div className={styles.wrap_hero_title}>
+        <h1 className={styles.title}>Make memories, one mile at a time</h1>
+        <NavLink className={styles.btn_go_catalog} to="/catalog">
+          Go to ads
+        </NavLink>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
