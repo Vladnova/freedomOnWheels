de2ea6894799ff05c552d7a5fc88ddabf3115e51
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2024-04-26 04:28:05 +0300

    add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,25 @@
-import MainLayouts from 'layouts/MainLayouts';
-import Favorites from 'pages/Favorites';
-import Home from 'pages/Home';
+
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Loader from './Loader';
-import NotFoundPage from 'pages/NotFoundPage';
 
 const Adverts = lazy(() => import('../pages/Adverts'));
+const Home = lazy(() => import('../pages/Home'));
+const Favorites = lazy(() => import('../pages/Favorites'));
+const MainLayouts = lazy(() => import('../layouts/MainLayouts'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayouts />}>
-        <Route index element={<Home />} />
-        <Route
-          path="/catalog"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Adverts />
-            </Suspense>
-          }
-        />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<MainLayouts />}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Adverts />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
--- src/layouts/MainLayouts/MainLayouts.jsx
+++ src/layouts/MainLayouts/MainLayouts.jsx
@@ -1,15 +1,11 @@
 import Header from 'components/Header';
-import Loader from 'components/Loader';
-import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 const MainLayouts = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={Loader}>
-        <Outlet />
-      </Suspense>
+      <Outlet />
     </>
   );
 };
